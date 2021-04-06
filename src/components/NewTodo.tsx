7fb2cdfd840e7294a3a8f1e284b57b2b30ad6339
Michael <stall84@gmail.com>
2021-04-06 07:03:15 -0400

    Added submit handler and text ref

--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useRef } from 'react'
 
 const NewTodo: React.FC = () => {
+
+    // useRef is a generic, so you'll need to define what type it will be of (HTMLInput Element)
+    const textInputRef = useRef<HTMLInputElement>(null);
+
+    const submitHandler = (event: React.FormEvent) => {
+        event.preventDefault();
+        const enteredText = textInputRef.current!.value;
+        console.log(textInputRef.current!.value);
+
+    }
+
     return (
-        <form>
-            FORM PLACEHOLDER
+        <form onSubmit={submitHandler}>
+            <div>
+                <label htmlFor="todo-text">
+                    Todo Text
+                </label>
+                <input type="text" id="todo-text" ref={textInputRef} />
+            </div>
+            <button type="submit">ADD TODO</button>
         </form>
     )
 }
